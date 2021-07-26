bac75d9c759e7087671e5dd667671d547d391d62
DESKTOP-5UF71SN\uberchilly <uberchilly@gmail.com>
2021-07-26 10:21:39 +0200

    Koin added to project for DI

--- starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -1,9 +1,14 @@
 package com.example.android.politicalpreparedness.election
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import com.example.android.politicalpreparedness.R
-
+import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
 class ElectionsFragment : Fragment(R.layout.fragment_election) {
+    private var _binding: FragmentElectionBinding? = null
+    private val binding get() = _binding!!
 
     //TODO: Declare ViewModel
 
@@ -25,6 +30,20 @@ class ElectionsFragment : Fragment(R.layout.fragment_election) {
 //
 //    }
 
-    //TODO: Refresh adapters when fragment loads
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentElectionBinding.bind(view)
+
+        with(binding){
+
+
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
